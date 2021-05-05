3db7f944c435e2f56ccb4179b25b31cfc0e6b5b9
Hasnain74 <33832359+Hasnain74@users.noreply.github.com>
2021-05-05 16:47:59 +0500

    Update _delete.blade.php

--- resources/views/admin/reports/_delete.blade.php
+++ resources/views/admin/reports/_delete.blade.php
@@ -1,1 +1,18 @@
+<a class="open-modal" data-url="{{route('reports.edit', $id)}}">
+    <button title="Edit" class="btn btn-primary">
+        <span class="fas fa-pencil-alt"></span>
+    </button>
+</a>
 
+ @permission('view.teachers_salary')
+<a data-toggle="modal" >
+    <button title="View" class="btn btn-warning">
+        <span class="fas fa-eye"></span>
+    </button>
+</a>
+@endpermission
+@permission('delete.teachers_salary')
+<a class="open-modal">
+    <button title="Delete" class="btn btn-danger"><span class="fas fa-trash-alt"></span></button>
+</a>
+@endpermission
