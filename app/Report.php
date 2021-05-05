59e5bcb18e56e37f4ec56591157628667a855857
Hasnain74 <33832359+Hasnain74@users.noreply.github.com>
2021-05-05 16:51:25 +0500

    Update Report.php

--- app/Report.php
+++ app/Report.php
@@ -32,6 +32,19 @@ class Report extends Model {
         'date',
         'year'
     ];
+    
+     public static function datatableColumns() {
+        return [
+            ['data' => 'checkbox', 'name' => 'checkbox', 'searchable'=> false, 'orderable'=> false, 'title' => '<input type="checkbox" id="dataTablesCheckbox">'],
+            ['data' => 'id', 'name' => 'id', 'title' => 'Id', 'className'=> 'text-center', 'visible' => false],
+            ['data' => 'student_id', 'name' => 'student_id', 'title' => 'Student ID', 'className'=> 'text-center'],
+            ['data' => 'student_name', 'name' => 'student_name', 'title' => 'Student Name', 'className'=> 'text-center'],
+            ['data' => 'report_categories_name', 'name' => 'report_categories_name', 'title' => 'Report Category', 'className'=> 'text-center'],
+            ['data' => 'subject', 'name' => 'subject', 'title' => 'Subject', 'className'=> 'text-center'],
+            ['data' => 'created_at', 'name' => 'created_at', 'title' => 'Created Date', 'className'=> 'text-center'],
+            ['data' => 'action', 'name' => 'action', 'title' => 'Actions', 'sortable' => false, 'className'=> 'text-center'],
+        ];
+    }
 
     public function studentsClass() {
         return $this->belongsTo('App\StudentsClass', 'class_id');
